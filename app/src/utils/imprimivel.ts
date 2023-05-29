d7c527ae27778dbd1179cf63a56ce1c26f8eb8ee
Ricardo Alves <dev.rical21@gmail.com>
2023-05-28 21:22:08 -0300

    05. Mais sobre Interfaces

---
+++ app/src/utils/imprimivel.ts
@@ -0,0 +1,6 @@
+export interface Imprimivel {
+  //Por padrão TODA interface é 'public'
+  //Por padrão TODA interface precisa ter seus métodos implementados 'implements'
+  //Por padrão TODO método de uma interface é 'abstract'
+  paraTexto(): string;
+}
